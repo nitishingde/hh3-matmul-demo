e6422afb3c0f58575c74aba2c667b74f2f152f65
Nitish Shingde <ngshingde@gmail.com>
2022-04-06 14:11:10 -0600

    COMM| Use 1d queue for comm matrix block data

--- comm_cpu_demo/task/accumulate_task.h
+++ comm_cpu_demo/task/accumulate_task.h
@@ -9,47 +9,43 @@ template<class Type, char Id, Order Ord>
 class AccumulateTask: public hh::AbstractTask<1, MatrixBlockData<Type, Id, Ord>, void*> {
 private:
     std::vector<std::vector<std::shared_ptr<MatrixBlockData<Type, Id, Ord>>>> rootMatrix_;
-    std::vector<std::vector<std::vector<std::shared_ptr<MatrixBlockData<Type, Id, Ord>>>>> queue_;
+    std::vector<std::shared_ptr<MatrixBlockData<Type, Id, Ord>>> queue_;
     std::atomic_uint32_t count_ = 0;
+    uint32_t rootCount_ = 0;
+    uint32_t rootSize;
     uint32_t expectedCount_;
-    std::mutex mutex_;
 
     bool doesRootMatrixExist(size_t row, size_t col) {
         return rootMatrix_[row][col] != nullptr;
     }
 
-    void execute(size_t row, size_t col) {
-        std::lock_guard lockGuard(mutex_);
+    void addAndUpdate(const std::shared_ptr<MatrixBlockData<Type, Id, Ord>> &other) {
+        uint32_t row = other->rowIdx(), col = other->colIdx();
         auto &c = rootMatrix_[row][col];
+
         if constexpr (Ord == Order::Row) {
-            for(auto &p: queue_[row][col]) {
-                for (size_t i = 0; i < c->blockSizeHeight(); ++i) {
-                    for (size_t j = 0; j < c->blockSizeWidth(); ++j) {
-                        c->blockData()[i * c->leadingDimension() + j] += p->get(i, j);
-                    }
+            for (size_t i = 0; i < c->blockSizeHeight(); ++i) {
+                for (size_t j = 0; j < c->blockSizeWidth(); ++j) {
+                    c->blockData()[i * c->leadingDimension() + j] += other->get(i, j);
                 }
-                count_++;
             }
+            count_++;
         }
         else {
-            for(auto &p: queue_[row][col]) {
-                for (size_t j = 0; j < c->blockSizeWidth(); ++j) {
-                    for (size_t i = 0; i < c->blockSizeHeight(); ++i) {
-                        c->blockData()[j * c->leadingDimension() + i] += p->get(j, i);
-                    }
+            for (size_t j = 0; j < c->blockSizeWidth(); ++j) {
+                for (size_t i = 0; i < c->blockSizeHeight(); ++i) {
+                    c->blockData()[j * c->leadingDimension() + i] += other->get(j, i);
                 }
-                count_++;
             }
+            count_++;
         }
-        queue_[row][col].clear();
     }
-
 public:
-    explicit AccumulateTask(size_t rows, size_t cols, size_t expectedCount):
+    explicit AccumulateTask(size_t rows, size_t cols, size_t partialCopies):
         hh::AbstractTask<1, MatrixBlockData<Type, Id, Ord>, void*>("AccumulateTask", 1, false),
-        expectedCount_(expectedCount) {
+        rootSize(rows*cols),
+        expectedCount_(rows*cols*partialCopies) {
         rootMatrix_.resize(rows, std::vector<std::shared_ptr<MatrixBlockData<Type, Id, Ord>>>(cols, nullptr));
-        queue_.resize(rows, std::vector<std::vector<std::shared_ptr<MatrixBlockData<Type, Id, Ord>>>>(cols));
     }
 
     void execute(std::shared_ptr<MatrixBlockData<Type, Id, Ord>> data) override {
@@ -58,17 +54,21 @@ public:
 
         if(data.get()->fullMatrixData() != nullptr) {
             rootMatrix_[rowIdx][colIdx] = data;
+            rootCount_++;
         }
-        else {
-            std::lock_guard lockGuard(mutex_);
-            queue_[rowIdx][colIdx].emplace_back(data);
-        }
-
-        if(doesRootMatrixExist(rowIdx, colIdx)) {
-            execute(rowIdx, colIdx);
+        else if(doesRootMatrixExist(rowIdx, colIdx)) {
+            addAndUpdate(data);
+        } else {
+            queue_.emplace_back(data);
         }
 
-        uint32_t temp = count_;
+        // one time clean up
+        if(!queue_.empty() and rootCount_ == rootSize) {
+            for(const auto &blockData: queue_) {
+                addAndUpdate(blockData);
+            }
+            queue_.clear();
+        }
         if(canTerminate()) {
             this->addResult(std::make_shared<void*>(nullptr));//FIXME
         }

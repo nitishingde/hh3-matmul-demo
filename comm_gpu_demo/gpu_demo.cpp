606da42b30ab4fc2bfc7ca983d575e5b0a1c8928
Nitish Shingde <ngshingde@gmail.com>
2022-07-06 09:32:52 -0600

    GPU| Allocate the verification matrix C when needed

--- comm_gpu_demo/gpu_demo.cpp
+++ comm_gpu_demo/gpu_demo.cpp
@@ -28,15 +28,11 @@ int main([[maybe_unused]]int32_t argc, [[maybe_unused]]char **argv) {
     size_t m = std::stoull(argv[1]), k = std::stoull(argv[2]), n = std::stoull(argv[3]), blockSize = std::stoull(argv[4]);
     printf("M = %zu, K = %zu, N = %zu, B = %zu\n", m, k, n, blockSize);
 
-    std::vector<MatrixType> A(m*k), B(k*n), C(m*n);
+    std::vector<MatrixType> A(m*k), B(k*n), C(m*n, 1);
 
     auto matrixA = std::make_shared<MatrixData<MatrixType, 'a', Ord>>(m, k, blockSize, *A.data());
     auto matrixB = std::make_shared<MatrixData<MatrixType, 'b', Ord>>(k, n, blockSize, *B.data());
     auto matrixC = std::make_shared<MatrixData<MatrixType, 'c', Ord>>(m, n, blockSize, *C.data());
-#if VERIFY_MM
-    std::vector<MatrixType> V(m*n);
-    auto testMatrixC = std::make_shared<MatrixData<MatrixType, 'c', Ord>>(m, n, blockSize, *V.data());
-#endif
 
     // Mersenne Twister Random Generator
     uint64_t timeSeed = std::chrono::system_clock::now().time_since_epoch().count();
@@ -48,20 +44,22 @@ int main([[maybe_unused]]int32_t argc, [[maybe_unused]]char **argv) {
     // initialize matrices
     std::for_each(matrixA->data(), matrixA->data() + (m * k), [&unif, &rng](MatrixType &val) { val = (MatrixType) unif(rng); });
     std::for_each(matrixB->data(), matrixB->data() + (k * n), [&unif, &rng](MatrixType &val) { val = (MatrixType) unif(rng); });
-    std::for_each(matrixC->data(), matrixC->data() + (m * n), [&unif, &rng](MatrixType &val) { val = 1;(MatrixType) unif(rng); });
-#if VERIFY_MM
-    for(size_t i = 0; i < m*n; ++i) {
-        testMatrixC->data()[i] = matrixC->data()[i];
-    }
-#endif
 
-    MMOuterProduct<MatrixType, Ord> mmOuterProduct;
-    mmOuterProduct.execute(matrixA, matrixB, matrixC);
+    std::cout << "Done initializing matrices" << std::endl;
+    {
+        MMOuterProduct<MatrixType, Ord> mmOuterProduct;
+        mmOuterProduct.execute(matrixA, matrixB, matrixC);
+    }
 
 #if VERIFY_MM
     // verify code
-    MMVerification<MatrixType, Ord> mmVerification;
-    mmVerification.execute(matrixA, matrixB, testMatrixC);
+    std::vector<MatrixType> V(m*n, 1);
+    auto testMatrixC = std::make_shared<MatrixData<MatrixType, 'c', Ord>>(m, n, blockSize, *V.data());
+
+    {
+        MMVerification<MatrixType, Ord> mmVerification;
+        mmVerification.execute(matrixA, matrixB, testMatrixC);
+    }
     for(size_t i = 0; i < m*n; ++i) {
         if(0.01 < std::abs(testMatrixC->data()[i]-matrixC->data()[i])) {
 #if not NDEBUG

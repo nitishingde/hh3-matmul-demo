97f8315e88eadf6c5f158ed1ed843d5cdcdbdf47
Nitish Shingde <ngshingde@gmail.com>
2022-09-25 01:28:58 -0600

    Refactor| Move verification and dumping of matrix data to matrix_utility header
    - fix bug in dumping data utility

--- dist_gpu_demo/notchpeak_outer_prod_1.cpp
+++ dist_gpu_demo/notchpeak_outer_prod_1.cpp
@@ -2,7 +2,7 @@
 #include "data/cyclic2d_matrix_container.h"
 #include "data/redundant_matrix_container.h"
 #include "data/contiguous_sub_matrix_container.h"
-#include "matrix_init.h"
+#include "matrix_utility.h"
 #include "mmd.h"
 #include <cstdio>
 
@@ -49,49 +49,13 @@ int main([[maybe_unused]]int32_t argc, [[maybe_unused]]char **argv) {
     }
 
 #if VERIFY_MMD
-    auto redundantMatrixC = std::make_shared<RedundantMatrixContainer<MatrixType, 'c', Ord>>(3, M, N, tileSize, matrixComm, isRootNodeId());
-    init(redundantMatrixC);
-
-    {
-        MPI_Barrier(MPI_COMM_WORLD);
-        MMD_VerifyCublas<MatrixType, 'a', 'b', 'c', Ord>().execute(std::move(subMatA), std::move(subMatB), redundantMatrixC, deviceIds);
-        subMatA = nullptr;
-        subMatB = nullptr;
-    }
-
-    MPI_Datatype datatype;
-    MPI_Type_contiguous(int32_t(M), std::is_same_v<MatrixType, double>? MPI_DOUBLE: MPI_FLOAT, &datatype);
-    MPI_Type_commit(&datatype);
-    MPI_Bcast(redundantMatrixC->data(), int32_t(N), datatype, 0, redundantMatrixC->mpiComm());
-    MPI_Barrier(MPI_COMM_WORLD);
-    if(isRootNodeId()) printf("Verifying solution.\n");
-
-    for(uint64_t i = 0; i < redundantMatrixC->matrixNumRowTiles(); ++i) {
-        for(uint64_t j = 0; j < redundantMatrixC->matrixNumColTiles(); ++j) {
-            if(auto tile = matrixC->getTile(i, j); tile == nullptr or tile->sourceNodeId() != getNodeId()) continue;
-            else if(*tile != *redundantMatrixC->getTile(i, j)) {
-                std::cerr << "[Error] tile @[" + std::to_string(i) + ", " + std::to_string(j) + "] don't match.\n";
-#if not NDEBUG
-                std::cout << GREEN("Actual  : ") << *redundantMatrixC->getTile(i, j);
-                std::cout <<   RED("Computed: ") << *tile;
-#endif
-            }
-        }
-    }
+    verifySolution(std::move(subMatA), std::move(subMatB), matrixC, deviceIds, matrixComm);
+    subMatA = nullptr;
+    subMatB = nullptr;
 #endif
 
 #if DUMP_DATA
-    for(uint64_t i = 0; i < matrixC->matrixNumColTiles(); ++i) {
-        for(uint64_t j = 0; j < matrixC->matrixNumRowTiles(); ++j) {
-            if(auto tile = matrixC->getTile(i, j); tile->sourceNodeId() == getNodeId()) {
-                std::string fileName = path+"/matrix_tile_"+std::to_string(i)+"_"+std::to_string(j)+".dat";
-                printf("[Process %d] Writing to file %s\n", getNodeId(), fileName.c_str());
-                auto pFile = fopen(fileName.c_str(), "w");
-                fwrite(tile->data(), sizeof(MatrixType), tile->dataSize(), pFile);
-                fclose(pFile);
-            }
-        }
-    }
+    dumpData(matrixC, path);
 #endif
 
     MPI_Comm_free(&matrixComm);
